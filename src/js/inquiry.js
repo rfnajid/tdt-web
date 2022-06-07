da232e442e0134d1eec08b7f647a2aa4536d1267
rida <ridhaaaaazis@gmail.com>
2022-06-08 01:20:46 +0700

    Add on-change listener

--- src/js/inquiry.js
+++ src/js/inquiry.js
@@ -6,6 +6,17 @@ console.log('inquiry js initiated');
 
 import { createApp } from "https://unpkg.com/petite-vue?module";
 createApp({
+    data:{
+        companyName:'',
+        companyAddress:'',
+        businessType:'',
+        numberOfEmployees:'',
+        companySellingPoint:'',
+        picName:'',
+        email:'',
+        phone:'',
+        jobSpec: []
+    },
     fields:{
         // COMPANY INFO
         companyName : {
@@ -13,7 +24,6 @@ createApp({
             label: 'Company name',
             placeholder: 'Your company name',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         companyAddress : {
@@ -21,7 +31,6 @@ createApp({
             label: 'Company address',
             placeholder: 'Your company main address',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         businessType : {
@@ -29,7 +38,6 @@ createApp({
             label: 'Type of Business',
             placeholder: 'FMCG, Finance, Construction, IT, Transportation, etc.',
             required: false,
-            value: null,
             gridSize: 'col-md-6'
         },
         numberOfEmployees : {
@@ -37,7 +45,6 @@ createApp({
             label: 'Number of Employees',
             placeholder: 'ex: 5, 10-20, >20',
             required: false,
-            value: null,
             gridSize: 'col-md-6'
         },
         companySellingPoint : {
@@ -45,7 +52,6 @@ createApp({
             label: 'Company Selling Point',
             placeholder: 'Unique Selling Preposition to candidates / What we should tell to candidates about your company',
             required: false,
-            value: null,
             gridSize: 'col-12'
         },
         // CONTACT
@@ -54,7 +60,6 @@ createApp({
             label: 'PIC Name',
             placeholder: 'PIC Name',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         email : {
@@ -62,7 +67,6 @@ createApp({
             label: 'Email',
             placeholder: 'example@gmail.com',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         phone : {
@@ -70,7 +74,6 @@ createApp({
             label: 'Phone number / Whatsapp',
             placeholder: '081234567890',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         // JOB SPEC
@@ -79,15 +82,13 @@ createApp({
             label: 'Job Position',
             placeholder: 'Software Engineer, Product Manager, Data Analyst',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         jobQuota : {
-            type: 'number',
+            type: 'text',
             label: 'Quota',
             placeholder: 'How many person do you need?',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         employementStatus : {
@@ -95,7 +96,6 @@ createApp({
             label: 'Employment Status',
             placeholder: 'Permanent, Contract, Freelance',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         workLocation : {
@@ -103,7 +103,6 @@ createApp({
             label: 'Work Location',
             placeholder: 'Office location',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         salaryRange : {
@@ -111,7 +110,6 @@ createApp({
             label: 'Salary Range',
             placeholder: '7.000.000 - 13.000.000',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         academicQualification : {
@@ -119,7 +117,6 @@ createApp({
             label: 'Academic Qualification',
             placeholder: 'SMA, D3, S1, S2',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         yearsOfExperience : {
@@ -127,7 +124,6 @@ createApp({
             label: 'Years of Experience',
             placeholder: '1 years, 3-5 years, fresh-grad',
             required: true,
-            value: null,
             gridSize: 'col-md-6'
         },
         skillSet : {
@@ -135,7 +131,6 @@ createApp({
             label: 'Skill Set (keyword)',
             placeholder: 'Java, MS Excel, Scrum etc',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         jobDescription : {
@@ -143,7 +138,6 @@ createApp({
             label: 'Job Description',
             placeholder: '',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         jobRequirement : {
@@ -151,7 +145,6 @@ createApp({
             label: 'Job Requirement',
             placeholder: '',
             required: true,
-            value: null,
             gridSize: 'col-12'
         },
         otherRequirement : {
@@ -159,13 +152,12 @@ createApp({
             label: 'Other Requirement',
             placeholder: '',
             required: false,
-            value: null,
             gridSize: 'col-12'
         },
     },
     sections:[
         {
-            id: 'company-profile',
+            id: 'companyProfile',
             label: 'Company Profile',
             deleteable: false,
             fields : [
@@ -182,13 +174,29 @@ createApp({
             ]
         },
     ],
-    jobSpecCount: 1,
     addJob (deletable=true) {
         console.log('add job clicked');
+        this.data.jobSpec.push({
+            jobPosition:'',
+            jobQuota:'',
+            employementStatus:'',
+            workLocation:'',
+            salaryRange:'',
+            academicQualification:'',
+            yearsOfExperience:'',
+            skillSet:'',
+            jobDescription:'',
+            jobRequirement:'',
+            otherRequirement:''
+        });
+        var jobSpecCount = this.data.jobSpec.length-1;
+        var jobSpecId = 'jobSpec'+jobSpecCount;
         var newJobSpec = {
-            id: 'job-spec-'+this.jobSpecCount,
-            label: 'Job Specification '+this.jobSpecCount,
+            id: jobSpecId,
+            label: 'Job Specification',
             deleteable: deletable,
+            isJobSpec: true,
+            jobSpecId: jobSpecCount,
             fields : [
                 'jobPosition', 'jobQuota','employementStatus',
                 'workLocation','salaryRange','academicQualification',
@@ -196,16 +204,32 @@ createApp({
                 'jobRequirement','otherRequirement'
             ]
         };
+       
         this.sections.push(newJobSpec);
-        this.jobSpecCount++;
+
+        console.log('jobSpec', JSON.stringify(this.data.jobSpec));
     },
     deleteSection(sectionId) {
         console.log('delete section', sectionId);
         this.sections = this.sections.filter(section => section.id!=sectionId);
+        this.data.jobSpec = delete this.data.jobSpec[sectionId];
+
+        console.log('sections', this.sections);
+        console.log('this.data.jobSpec', this.data.jobSpec);
     },
     onSubmit(){
-        console.log('submit');
-        console.log('this', this);
+        console.log('submit data', JSON.stringify(this.data));
+        console.log('suthis.data.jobSpec', JSON.stringify(this.data.jobSpec));
+    },
+    onChange(event, section, fieldName){
+        var value = event.target.value;
+        if(section.isJobSpec){
+            this.data.jobSpec[section.jobSpecId][fieldName] = value;
+        }else{
+            this.data[fieldName] = value;
+        }
+
+        console.log('change', JSON.stringify(this.data));
     },
     onMounted() {
         console.log('inquiry mounted');
